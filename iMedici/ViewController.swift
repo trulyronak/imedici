dff98cd4cb614d9e4f01c235f461289e10ac6d74
Ronak Shah <ronakshah@shahindustries.one>
2016-02-02 00:03:49 -0800

    Initial Commit

---
+++ iMedici/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iMedici
+//
+//  Created by Ronak Shah on 2/1/16.
+//  Copyright © 2016 Shah Industries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
